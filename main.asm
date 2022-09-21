a3bc7475c8f27232352dc879cd5aa477897d3bf3
chopsticks-user <huuquangcap@gmail.com>
2022-09-21 08:57:23 -0400

    Added fib to math.inc as fib_64

--- main.asm
+++ main.asm
@@ -1,6 +1,6 @@
 ; x86_64 Intel Assembly
 
-%include "include/debug.inc"
+%include "include/math.inc"
 
 section .data
         dec_fm: db "%d", 0x0a
@@ -8,42 +8,12 @@ section .bss
 section .text
         extern printf
 
-global fibonacci
-fibonacci:
-        cmp rdi, 0              ; if (n <= 0)
-        jle .c0
-        cmp rdi, 2              ; if (n <= 2)
-        jle .c12
-
-        mov rcx, 3              ; i = 3
-        mov rax, 2              ; current = 1 (last1)
-        mov rdx, 1              ; last1 = 1
-        mov rsi, 1              ; last2 = 1
-        jmp .cnd                
-.lp:
-        mov rdx, rax            ; last1 = current
-        add rax, rsi            ; current += last2
-        mov rsi, rdx            ; last2 = last1
-        inc rcx                 ; ++i
-        
-.cnd
-        cmp rcx, rdi
-        jne .lp
-        ret
-.c12
-        mov rax, 1
-        ret
-.c0:
-        xor rax, rax
-        ret
-
 global main
 main:
         push rbp
         mov rbp, rsp
 
-        mov rdi, 500
-        call fibonacci
+        fib_64 500
 
         lea rdi, [dec_fm]
         mov rsi, rax
